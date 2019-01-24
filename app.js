716fabcb2811b26115532a7d9797b2ed3278fd74
Sreekant <geekykant@gmail.com>
2019-01-24 22:40:24 +0530

    routing pages

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require('express');
-var app = express();
+var db = require('./db/db');
+var parser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello world!'));
+var app = express();
+app.use(parser.urlencoded({
+  extended: true
+}));
+
+var admin = express();
+admin.get('/', (req, res) => {
+  res.send('admin homepage!');
+});
+
+var secret = express();
+secret.get('/', (req, res) => {
+  res.send('admin secr*t');
+});
+
+admin.use(parser.urlencoded({
+  extended: true
+}));
+
+
+app.use(['/admin', '/manager'], admin);
+admin.use('/secr*t', secret);
+
 
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port 3000!")
+  console.log(`Server running on port ${PORT}!`)
 });
